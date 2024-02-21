572f21021ebcef6fd9ad47fcaaf4061e130dbbe9
Kba <struys.wyvin@gmail.com>
2024-02-21 16:13:22 +0100

    exo-01 - Définition des routes

---
+++ src/app/articles/page.js
@@ -0,0 +1,10 @@
+export default function Articles(){
+    return(
+        <main className="main-container">
+            <h1 className="main-title">Liste des articles</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda at aut consectetur culpa cum cupiditate delectus eaque enim excepturi exercitationem expedita fugiat harum illo, libero magni minima molestias mollitia nihil nostrum odit officiis porro praesentium quam rerum sint tenetur. Consequuntur et id illum maiores maxime natus provident temporibus vel.
+            </p>
+        </main>
+    )
+}
---
+++ src/app/contact/page.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return(
+        <main className="main-container">
+            <h1 className="main-title">Contactez nous !</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, cumque distinctio dolorum eos facere fugit in inventore nesciunt nulla, perferendis possimus quae recusandae ullam vero!</p>
+        </main>
+    )
+}
