92a09147b0266f098c9c16f0f5d1fe5d0a18b2f4
Kba <struys.wyvin@gmail.com>
2024-02-24 03:15:58 +0100

    exo-08

--- src/app/articles/page.js
+++ src/app/articles/page.js
@@ -4,7 +4,7 @@ export default function Articles(){
     return(
         <main className="main-container">
             <h1 className="main-title">Liste des articles</h1>
-            <p>
+            <p className="mb-5">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda at aut consectetur culpa cum cupiditate delectus eaque enim excepturi exercitationem expedita fugiat harum illo, libero magni minima molestias mollitia nihil nostrum odit officiis porro praesentium quam rerum sint tenetur. Consequuntur et id illum maiores maxime natus provident temporibus vel.
             </p>
             <ArticleList />
